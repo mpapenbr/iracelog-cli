2e36da41b1ec0ba510b474489b35ac62e4570883
mpapenbr <Markus.Papenbrock+github@gmail.com>
2026-08-22 14:45:35 +0200

    Use sha references for Github actions Fixes #190

--- devinit.sh
+++ devinit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "devinit.sh called"
 
+make install
 
-go install github.com/goreleaser/goreleaser/v2@latest
+# keep this here in comments as a source if we need to install it on demand
+# go install github.com/goreleaser/goreleaser/v2@latest
 # go install github.com/caarlos0/svu@latest
 
 if [ -f setuplinks.sh ]; then
